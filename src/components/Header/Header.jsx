af9fa30a6fccf7ce1a65adf2a3427682ea282a7a
ariadnatoledo <ariadnatoledo1@gmail.com>
2024-12-28 09:38:31 -0800

    Add Typography, mixins and variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,12 @@ function Header() {
   return (
     <div className="header">
     <h1 className="header__title">Ariadna Toledo - Software Engineer</h1>
+    <h2 className="header__title">Ariadna Toledo - Software Engineer</h2>
+    <h3 className="header__title">Ariadna Toledo - Software Engineer</h3>
+    <h4 className="header__title">Ariadna Toledo - Software Engineer</h4>
+    <p className="header__title">Ariadna Toledo - Software Engineer</p>
+    <span className="header__title">Ariadna Toledo - Software Engineer</span>
+
     </div>
   )
 }
